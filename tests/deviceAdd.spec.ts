23f223db68f435f55e71c466c1bd3950e201f9b0
Sreejithdcube <82438604+Sreejithdcube@users.noreply.github.com>
2025-04-23 15:21:58 +0530

    Added serial in device add

--- tests/deviceAdd.spec.ts
+++ tests/deviceAdd.spec.ts
@@ -6,15 +6,17 @@ test.beforeEach(async ({ page }) => {
   await loginPage.goto();
   await loginPage.login('admin@dcubeai.com', 'DcubeAdmin@123');
 });
-test('Create Device in DMON', async ({ page }) => {
-  const devicePage = new Devices(page);
-  await devicePage.openDeviceForm();
-  await devicePage.fillDeviceForm('1233', 'test222', 'sreejithteck@gmail.com');
-  await devicePage.submitForm();
-});
-test('Delete a specific device by ID', async ({ page }) => {
-  
-  const devicePage = new Devices(page);
-  await devicePage.gotoDevices();
-  await devicePage.deleteDeviceById('1233');
+test.describe.serial('Device Flow', () => {
+  test('Create Device in DMON', async ({ page }) => {
+    const devicePage = new Devices(page);
+    await devicePage.openDeviceForm();
+    await devicePage.fillDeviceForm('1233', 'test222', 'sreejithteck@gmail.com');
+    await devicePage.submitForm();
+  });
+  test('Delete a specific device by ID', async ({ page }) => {
+
+    const devicePage = new Devices(page);
+    await devicePage.gotoDevices();
+    await devicePage.deleteDeviceById('1233');
+  });
 });
